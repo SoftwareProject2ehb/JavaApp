5ddae2af78b0856f847cae2609c80ea4625db697
MHHel <mohamed.helalouch@outlook.com>
2016-11-07 12:08:31 +0100

    added find method by attribute

--- SoftwareProject/src/data_control/UserDAO.java
+++ SoftwareProject/src/data_control/UserDAO.java
@@ -7,12 +7,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import model.Customer;
 import model.User;
 import model.User.Role;
 
 public class UserDAO extends BaseDAO{
 	
-	public void addUser(User user) {
+	public void createUser(User user) {
 		PreparedStatement ps = null;
 
 		String sql = "INSERT INTO User VALUES(?,?,?,?,?,?,?,?,?)";
@@ -58,7 +59,7 @@ public class UserDAO extends BaseDAO{
 	public void updateUser(User user) {
 		
 		PreparedStatement ps = null;	
-		String update = "UPDATE User SET first_name=?, last_name=?, email=?, phone=?, login=?, password=? role=? active=? WHERE ID = ?";
+		String update = "UPDATE User SET first_name=?, last_name=?, email=?, phone=?, login=?, password=?, role=?, active=? WHERE ID = ?";
 		
 		try {
 		if (getCon().isClosed()) {
@@ -67,16 +68,17 @@ public class UserDAO extends BaseDAO{
 			ps = getCon().prepareStatement(update);
 		
 	
-			ps.setInt(1, user.getUserID());
-			ps.setString(2, user.getFirstName());
-			ps.setString(3, user.getLastName());
-			ps.setString(4, user.getEmail());
-			ps.setString(5, user.getPhone());
+			
+			ps.setString(1, user.getFirstName());
+			ps.setString(2, user.getLastName());
+			ps.setString(3, user.getEmail());
+			ps.setString(4, user.getPhone());
 			//ps.setString(6, user.getAddress());
-			ps.setString(6, user.getLogin());
-			ps.setString(7, user.getPassword());
-			ps.setString(8, user.getRolen());
-			ps.setBoolean(9, user.isActive());
+			ps.setString(5, user.getLogin());
+			ps.setString(6, user.getPassword());
+			ps.setString(7, user.getRolen());
+			ps.setBoolean(8, user.isActive());
+			ps.setInt(9, user.getUserID());
 		
 			ps.executeUpdate();
 			ps.close();
@@ -87,7 +89,7 @@ public class UserDAO extends BaseDAO{
 		}      
 	}
 	
-	public User delUser(int userID) {
+	public User removeUser(int userID) {
 		Statement st = null;
 		User user = null;
 		try {
@@ -106,19 +108,17 @@ public class UserDAO extends BaseDAO{
 		return user;
 	}
 	
-	public ArrayList<User> searchUserByLastName(String lastName ) {
-		ArrayList<User> lijst = new ArrayList<User>();
+	public User findUserById(int id) {
+		User user = null;
 		Statement st = null;
 		try {
-			if (getCon() == null || getCon().isClosed()) {
-				// afhandelen zoals je zelf wilt
-				throw new IllegalStateException("Connection onverwacht beeindigd");
-			}
-			st = getCon().createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM User WHERE last_name IN ('" + lastName + "') ");
-
-			while (rs.next()) {
-				User u = new User(
+	        if (getCon().isClosed()) {
+	            throw new IllegalStateException("error unexpected");
+	        }
+	        st = (Statement) getCon().createStatement();
+	        ResultSet rs = st.executeQuery("SELECT * FROM User WHERE ID = " + id);
+	        if (rs.next()) {
+	        	user = new User(
 						rs.getInt("ID"), 
 						rs.getString("first_name"),
 						rs.getString("last_name"), 
@@ -129,14 +129,20 @@ public class UserDAO extends BaseDAO{
 						rs.getString("password"), 
 						Role.valueOf(rs.getString("role")), 
 						rs.getBoolean("active"));
-				lijst.add(u);
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return lijst;
+	    } catch (SQLException e) {
+	        System.out.println(e.getMessage());
+	        throw new RuntimeException(e.getMessage());
+	    } finally {
+	        try {
+	            if (st != null)
+	            	st.close();
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	            throw new RuntimeException("error.unexpected");
+	        }
+	    }
+		return user;
 	}
 	
 	public ArrayList<User> getAllUsers() {
@@ -171,16 +177,77 @@ public class UserDAO extends BaseDAO{
 		return lijst;
 	}
 
+	enum FindUser {ID,first_name,last_name,email,phone,login ,password,role,active};
+
+	public ArrayList<User> findUserByAttribute(FindUser attribuut,String zoekop) { //Find user by attribute and string
+		ArrayList<User> lijst = new ArrayList<User>();
+		Statement st = null;
+		try {
+			if (getCon().isClosed()) {
+				throw new IllegalStateException("error unexpected");
+			}
+			st = (Statement) getCon().createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM User WHERE " + attribuut + " IN ('" + zoekop + "') ");
+
+			while (rs.next()) {
+				User u = new User(rs.getInt("ID"), 
+						rs.getString("first_name"),
+						rs.getString("last_name"), 
+						rs.getString("email"),
+						rs.getString("phone"),
+						//rs.getString("address"), 
+						rs.getString("login"),
+						rs.getString("password"), 
+						Role.valueOf(rs.getString("role")), 
+						rs.getBoolean("active"));
+				lijst.add(u);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lijst;
+	}
 	
+	public ArrayList<User> findUserByAttribute(FindUser attribuut,int zoekop) { //Find user by attribute and integer
+		ArrayList<User> lijst = new ArrayList<User>();
+		Statement st = null;
+		try {
+			if (getCon().isClosed()) {
+				throw new IllegalStateException("error unexpected");
+			}
+			st = (Statement) getCon().createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM User WHERE " + attribuut + " = " + zoekop);
+
+			while (rs.next()) {
+				User u = new User(rs.getInt("ID"), 
+						rs.getString("first_name"),
+						rs.getString("last_name"), 
+						rs.getString("email"),
+						rs.getString("phone"),
+						//rs.getString("address"), 
+						rs.getString("login"),
+						rs.getString("password"), 
+						Role.valueOf(rs.getString("role")), 
+						rs.getBoolean("active"));
+				lijst.add(u);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lijst;
+	}
 	
+
 	public static void main(String[] args) {
 		UserDAO ud = new UserDAO();
-		//User u = new User(0,"Mohamed","Helalouch","mohamed.helalouch@student.ehb.be","048981991","mohhel", "pass",Role.valueOf("ADMIN"),true); 
+		//User u = new User(0,"Mohamed","Blabla","mohamed.blabla@student.ehb.be","03812391","mohb", "test",Role.valueOf("USER"),true); 
 		//ud.addUser(u);
 		  
-		ArrayList<User> list = ud.searchUserByLastName("Helalouch");
-			for(User bn: list){
-				System.out.println(bn.getEmail()+" , " + bn.isActive());
+		ArrayList<User> list = ud.findUserByAttribute(FindUser.valueOf("role"),"USER");
+		for(User bn: list){
+			System.out.println(bn.getFirstName() + " , " + bn.getLastName() + " , " + bn.getEmail() + " , " + bn.getRolen());
 		}
 		
 	  }
