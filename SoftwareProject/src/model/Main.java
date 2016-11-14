97def6926e8d9f90234b46ddfa47a3a2f633541d
Runekid <Runekid@localhost>
2016-11-14 08:45:08 +0100

    klasse ApiAccesser aangemaakt voor informatie op te vragen van de api.

--- SoftwareProject/src/model/Main.java
+++ SoftwareProject/src/model/Main.java
@@ -1,10 +1,14 @@
 package model;
+import utilities.*;
 
-
+import java.io.IOException;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import data_control.*;
 import model.Price;
@@ -19,13 +23,16 @@ import data_control.*;
 public class Main {
 
 
-	public static void main(String[] args) throws ParseException {
-		SubscriptionDAO ad = new SubscriptionDAO();
-		String startd = "2016-10-13 12:33:12";
-		String endd = "2016-10-25 14:33:12";
-		
-		//hi
-		Subscription a = ad.findSubById(2);
-		ad.setActive(a);
+	public static void main(String[] args) throws ParseException, JSONException, IOException {
+		JSONObject json = ApiAccesser.readJsonFromUrl("https://traintracks.online/api/Route/Zele/Brussel-Zuid");
+		System.out.println("Jouw Verbindingen: Zele - Brussel-Zuid");
+		/*
+		JSONArray msg = (JSONArray) json.get("Routes");
+		JSONObject ob1 = msg.getJSONObject(0);
+		*/
+		System.out.println(json.get("StepOn"));
+		System.out.println(json.get("StepOff"));
+		System.out.println();
+		System.out.println(json.get("Routes"));
 	}
 }
