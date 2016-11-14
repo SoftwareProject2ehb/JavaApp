3402924dba71b569f8ec87b8f30dcd9a7af61e71
bernd.wethmar <bernd.wethmar@DT-C209-PC46.ehb.local>
2016-11-14 12:19:20 +0100

    Removed Main implementation

--- SoftwareProject/src/model/Main.java
+++ SoftwareProject/src/model/Main.java
@@ -18,7 +18,6 @@ import data_control.*;
 public class Main {
 
 	public static void main(String[] args) {
-		CustomerDAO.createCustomer(new Customer("Bernd", "Wethmar", "a@w.be", "007", "Straatnaam"));
 
 	}
 }
