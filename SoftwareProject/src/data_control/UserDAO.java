f13d59e799c05f6b2b2a4701033dd678d3bea4d7
MHHel <mohamed.helalouch@outlook.com>
2016-11-19 20:45:08 +0100

    example for checkPassword

--- SoftwareProject/src/data_control/UserDAO.java
+++ SoftwareProject/src/data_control/UserDAO.java
@@ -1,16 +1,13 @@
 package data_control;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import model.Customer;
 import model.User;
 import model.User.Role;
-import utilities.Encryptor;
 
 public class UserDAO extends BaseDAO{
 	
