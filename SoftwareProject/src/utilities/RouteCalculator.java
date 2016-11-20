15ffbea8fc75189e231c1da71851564cfba384e5
Bernd <Bernd@10.100.0.80>
2016-11-20 19:45:43 +0100

    Werk aan RouteCalculator

---
+++ SoftwareProject/src/utilities/RouteCalculator.java
@@ -0,0 +1,23 @@
+package utilities;
+
+public final class RouteCalculator {
+
+	private RouteCalculator() {
+	}
+	
+	public static double calculateDistance(String station_1, String station_2) {
+		return 0;
+	}
+	
+	public static int calculateStations(String station_1, String station_2) {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @return The value in hours, with minutes and seconds as a fraction of hours
+	 */
+	public static double calculateTime(String station_1, String station_2) {
+		return 0;
+	}
+}
