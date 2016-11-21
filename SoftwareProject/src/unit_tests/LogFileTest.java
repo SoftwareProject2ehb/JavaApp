4b4c92fcf30b5cd0372a53a5f07bb6403db686fe
loekes <lukas.raymaekers@student.ehb.be>
2016-11-21 10:28:24 +0100

    logfile, logfileDAO + tests

---
+++ SoftwareProject/src/unit_tests/LogFileTest.java
@@ -0,0 +1,54 @@
+package unit_tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.LogFile;
+
+public class LogFileTest {
+	
+	private LogFile log;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConstructor() {
+		Date date = new Date();
+		log = new LogFile("aanmaken ticket", date, 1);
+		assertTrue(log.getDescription().equals("aanmaken ticket"));
+		assertTrue(log.getTime().equals(date));
+		assertTrue(log.getUserID() == 1);
+		assertTrue(log.getLogFileID() == -1);
+	}
+	
+	@Test 
+	public void testGettersAndSetters() {
+		Date date = new Date();
+		log = new LogFile("aanmaken ticket", date, 1);
+		log.setDescription("aanmaken user");
+		assertTrue(log.getDescription().equals("aanmaken user"));
+		
+		long time = 7000000000000L;
+		Date date2 = new Date(time);
+		log.setTime(date2);
+		assertTrue(log.getTime().equals(date2));
+		
+		log.setLogFileID(100);
+		assertTrue(log.getLogFileID() == 100);
+		
+		log.setUserID(33);
+		assertTrue(log.getUserID() == 33);
+	}
+
+}
