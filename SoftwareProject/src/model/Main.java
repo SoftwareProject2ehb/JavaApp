0b71b22a746ffd4ccbc504d55695f7c4aa00eedd
loekes <lukas.raymaekers@student.ehb.be>
2016-12-26 15:22:45 +0100

    fix for conflicts #1

--- SoftwareProject/src/model/Main.java
+++ SoftwareProject/src/model/Main.java
@@ -8,7 +8,7 @@ import utilities.Language.LANGUAGE;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) {
 		SystemController.startUp();
 	}
 }
