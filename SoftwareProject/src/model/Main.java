8b0c6a9cfe7a1b7ab02caaf5e23bebbf346e07d5
Bernd <bernd@wethmar.be>
2017-01-02 21:09:45 +0100

    Final version

--- SoftwareProject/src/model/Main.java
+++ SoftwareProject/src/model/Main.java
@@ -1,11 +1,10 @@
 package model;
 
 import controller.*;
-import utilities.Cacher;
 
 public class Main {
 
 	public static void main(String[] args){
-		SystemController.startUp();
+		SystemController.startUp();		
 	}
 }
